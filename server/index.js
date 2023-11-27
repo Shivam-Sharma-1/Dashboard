849b583f8786d8cc1d73bddcf6518018d800d60c
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-27 13:15:55 +0530

    feat: Add routes and setup MongoDB connection

--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 
+import generalRoutes from "./routes/generalRoutes.js";
+import clientRoutes from "./routes/clientRoutes.js";
+import managementRoutes from "./routes/managementRoutes.js";
+import salesRoutes from "./routes/salesRoutes.js";
+import mongoose from "mongoose";
+
 // CONFIGURATION
 dotenv.config();
 const app = express();
@@ -21,3 +27,19 @@ app.use("/general", generalRoutes);
 app.use("/client", clientRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
