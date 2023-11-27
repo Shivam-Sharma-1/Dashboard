b9e7605f3300ab014569a0f40b440ff1b06102b7
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-27 19:45:25 +0530

    refactor: FlexBetween component

--- client/src/components/FlexBetween.jsx
+++ client/src/components/FlexBetween.jsx
@@ -1,10 +1,10 @@
 import { styled } from "@mui/system";
 import { Box } from "@mui/material";
 
-const FLexBetween = styled(Box)({
+const FlexBetween = styled(Box)({
   display: "flex",
   justifyContent: "space-between",
   alignItems: "center",
 });
 
-export default FLexBetween;
+export default FlexBetween;
