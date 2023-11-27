3b8b5daad2534e15d18171037486213cf9e21a1e
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-27 16:13:31 +0530

    feat: Add FlexBetween component

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { styled } from "@mui/system";
+import { Box } from "@mui/material";
+
+const FLexBetween = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+export default FLexBetween;
