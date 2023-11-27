2a8fc0b11473654dbe79304872e98300dcf699ca
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-27 20:50:01 +0530

    refactor: userSchema variable name in User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
+const UserSchema = new mongoose.Schema(
   {
     name: {
       type: String,
@@ -34,6 +34,6 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", UserSchema);
 
 export default User;
