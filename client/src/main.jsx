8d57dc722a36ae1a09cf9b3ea1b989604852c0fd
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-27 15:43:24 +0530

    refactor: main.jsx to use Redux for global state management

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.css";
+import App from "./App.jsx";
+import globalReducer from "./state";
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+  },
+});
+
+export default store;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
