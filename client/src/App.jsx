a777e943e57b2b1388cae5d6d4b26905cc6fa6d7
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-27 16:14:03 +0530

    feat: Add theme and router configuration to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,38 @@
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+
+import { themeSettings } from "./theme";
+import Layout from "./scenes/Layout";
+import Dashboard from "./scenes/Dashboard";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Layout />}>
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Route>
+  )
+);
+
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
       <h1>App</h1>
-    </>
+    </ThemeProvider>
   );
 }
 
