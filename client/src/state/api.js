5ba8e0fd17448f6fe17c01d530b6f954ddef0e37
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-27 21:20:10 +0530

    fix: import.meta.url.VITE_APP_BASE_URL to import.meta.env.VITE_APP_BASE_URL

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.url.VITE_APP_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_APP_BASE_URL }),
   reducerPath: "adminApi",
   tagTypes: ["User"],
   endpoints: (builder) => ({
