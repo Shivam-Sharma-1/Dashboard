0abdeae407db7883883e3dab327762c7bcba45a1
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-27 19:45:51 +0530

    refactor: layout component to include Navbar and Sidebar

--- client/src/scenes/Layout.jsx
+++ client/src/scenes/Layout.jsx
@@ -1,5 +1,30 @@
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+import { Box, useMediaQuery } from "@mui/material";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+
 function Layout() {
-  return <div>layout</div>;
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  return (
+    <Box width="100%" height="100%" display={isNonMobile ? "flex" : "block"}>
+      <Sidebar
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box flexGrow={1}>
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+  );
 }
 
 export default Layout;
