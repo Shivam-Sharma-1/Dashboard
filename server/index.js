855c0bbc4c232595e20bf4265274ab46e8b6ba9b
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-27 12:58:28 +0530

    Add routes for general, client, management, and sales

--- server/index.js
+++ server/index.js
@@ -15,3 +15,9 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+// ROUTES
+app.use("/general", generalRoutes);
+app.use("/client", clientRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
