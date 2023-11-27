fdc61cd15b9d2a968b1ddc598e26103722da03b4
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-27 22:12:18 +0530

    feat: Add user data to Sidebar and Navbar components

--- client/src/scenes/Layout.jsx
+++ client/src/scenes/Layout.jsx
@@ -3,14 +3,20 @@ import Sidebar from "@/components/Sidebar";
 import { Box, useMediaQuery } from "@mui/material";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useGetUserByIdQuery } from "@/state/api";
 
 function Layout() {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state) => state.global.userId);
+  const { data } = useGetUserByIdQuery(userId);
+  console.log(data);
 
   return (
     <Box width="100%" height="100%" display={isNonMobile ? "flex" : "block"}>
       <Sidebar
+        user={data || {}}
         isNonMobile={isNonMobile}
         drawerWidth="250px"
         isSidebarOpen={isSidebarOpen}
@@ -18,6 +24,7 @@ function Layout() {
       />
       <Box flexGrow={1}>
         <Navbar
+          user={data || {}}
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}
         />
