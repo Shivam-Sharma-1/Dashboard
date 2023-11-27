92e060d3acc8de49e64e88ae1504be40d7ab672d
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-27 20:50:46 +0530

    feat: Add Redux Toolkit API middleware and reducer

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 import "./index.css";
 import App from "./App.jsx";
 import globalReducer from "./state";
+import { api } from "./state/api";
 
 const store = configureStore({
   reducer: {
     global: globalReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
-
-export default store;
+setupListeners(store.dispatch);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
