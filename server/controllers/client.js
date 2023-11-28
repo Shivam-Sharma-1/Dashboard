580e7fc89bb8d5a2806f66c813446025a89dbef9
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-28 13:29:57 +0530

    feat: Add getCustomers endpoint

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,5 +1,6 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js";
 
 export const getProducts = async (req, res) => {
   try {
@@ -18,6 +19,16 @@ export const getProducts = async (req, res) => {
     );
 
     res.status(200).json(productsWithStats);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getCustomers = async (req, res) => {
+  try {
+    const customers = await User.find({ role: "user" }).select("-password");
+
+    res.status(200).json(customers);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
