5988f87318e7b2f25b51f5657a5e23468fcb5450
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-28 22:11:40 +0530

    feat: Add getSales function to retrieve overall sales statistics

---
+++ server/controllers/sales.js
@@ -0,0 +1,12 @@
+import OverallStat from "../models/OverallStat.js";
+
+export const getSales = async (req, res) => {
+  try {
+    const overallStats = await OverallStat.find();
+
+    res.status(200).json(overallStats[0]);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
+  }
+};
