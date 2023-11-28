92286760193da3f5af91cf15b3832c1ab7a8ac90
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-28 20:21:02 +0530

    feat: Add "Geography" tag type to API

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_APP_BASE_URL }),
   reducerPath: "adminApi",
-  tagTypes: ["User", "Products", "Customers", "Transactions"],
+  tagTypes: ["User", "Products", "Customers", "Transactions", "Geography"],
   endpoints: (builder) => ({
     getUserById: builder.query({
       query: (id) => `general/user/${id}`,
@@ -30,6 +30,10 @@ export const api = createApi({
       }),
       providesTags: ["Transactions"],
     }),
+    getGeography: builder.query({
+      query: () => `client/geography`,
+      providesTags: ["Geography"],
+    }),
   }),
 });
 
@@ -38,4 +42,5 @@ export const {
   useGetProductsQuery,
   useGetCustomersQuery,
   useGetTransactionsQuery,
+  useGetGeographyQuery,
 } = api;
