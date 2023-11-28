b79ddba45e8e7cb7e10603b19a79edf91a6d4aab
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-28 18:42:51 +0530

    feat: Add getTransactions route to clientRoutes.js

--- server/routes/clientRoutes.js
+++ server/routes/clientRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getCustomers, getProducts } from "../controllers/client.js";
+import {
+  getCustomers,
+  getProducts,
+  getTransactions,
+} from "../controllers/client.js";
 
 const router = express.Router();
 
 router.get("/products", getProducts);
 router.get("/customers", getCustomers);
+router.get("/transactions", getTransactions);
 
 export default router;
