1c9fd404c18b6a6850f5b2eb93c802a8d3161764
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-28 10:54:27 +0530

    feat: Add "getProducts" query to API

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,13 +3,17 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_APP_BASE_URL }),
   reducerPath: "adminApi",
-  tagTypes: ["User"],
+  tagTypes: ["User", "Products"],
   endpoints: (builder) => ({
     getUserById: builder.query({
       query: (id) => `general/user/${id}`,
       providesTags: ["User"],
     }),
+    getProducts: builder.query({
+      query: () => `client/products`,
+      providesTags: ["Products"],
+    }),
   }),
 });
 
-export const { useGetUserByIdQuery } = api;
+export const { useGetUserByIdQuery, useGetProductsQuery } = api;
