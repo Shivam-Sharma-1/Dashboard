d5922596ef408b7c8e8d26dfeb66ed2018bebd13
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-28 13:30:10 +0530

    feat: Add route for customers

--- server/routes/clientRoutes.js
+++ server/routes/clientRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getProducts } from "../controllers/client.js";
+import { getCustomers, getProducts } from "../controllers/client.js";
 
 const router = express.Router();
 
 router.get("/products", getProducts);
+router.get("/customers", getCustomers);
 
 export default router;
