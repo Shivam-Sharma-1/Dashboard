db1003a35da34391607633e562a51cdf5548d5f1
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-28 22:11:26 +0530

    feat: Add OverallStat model

---
+++ server/models/OverallStat.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const OverallStatSchema = new mongoose.Schema(
+  {
+    totalCustomers: Number,
+    yearlySalesTotal: Number,
+    yearlyTotalSoldUnits: Number,
+    year: Number,
+    monthlyData: [
+      {
+        month: String,
+        totalSales: Number,
+        totalUnits: Number,
+      },
+    ],
+    dailyData: [
+      {
+        day: String,
+        totalSales: Number,
+        totalUnits: Number,
+      },
+    ],
+    salesByCategory: {
+      type: Map,
+      of: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+const OverallStat = new mongoose.model("OverallStat", OverallStatSchema);
+export default OverallStat;
