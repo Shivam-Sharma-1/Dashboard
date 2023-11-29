0a1bfbf0e5e443cb9fb1917eacc865c31f5412dd
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-29 10:47:07 +0530

    chore: inject data

--- server/index.js
+++ server/index.js
@@ -11,7 +11,12 @@ import clientRoutes from "./routes/clientRoutes.js";
 import managementRoutes from "./routes/managementRoutes.js";
 import salesRoutes from "./routes/salesRoutes.js";
 import Product from "./models/Product.js";
-import { dataOverallStat, dataProduct, dataTransaction } from "./data/index.js";
+import {
+  dataOverallStat,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+} from "./data/index.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
@@ -42,7 +47,7 @@ mongoose
       console.log(`Server running on port: ${PORT}`);
     });
     // Product.insertMany(dataProduct)
-    // ProductStat.insertMany(dataProductStat)
+    // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
     // OverallStat.insertMany(dataOverallStat);
   })
