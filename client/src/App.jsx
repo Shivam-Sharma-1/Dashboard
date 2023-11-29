cdd40ed814705a95b1dc56cab75bce8f9e7f1886
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-29 11:14:45 +0530

    feat: Add Breakdown scene to router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,6 +20,7 @@ import Geography from "./scenes/Geography";
 import Overview from "./scenes/Overview";
 import Daily from "./scenes/Daily";
 import Monthly from "./scenes/Monthly";
+import Breakdown from "./scenes/Breakdown";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -33,6 +34,7 @@ const router = createBrowserRouter(
       <Route path="/overview" element={<Overview />} />
       <Route path="/daily" element={<Daily />} />
       <Route path="/monthly" element={<Monthly />} />
+      <Route path="/breakdown" element={<Breakdown />} />
     </Route>
   )
 );
