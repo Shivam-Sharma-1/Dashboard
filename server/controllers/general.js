2f8f7f20c76c3f6bd599012a48363a5c695d2b9f
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-30 19:11:07 +0530

    feat: Add OverallStat and Transaction imports, and create getDashboardStats endpoint

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,3 +1,5 @@
+import OverallStat from "../models/OverallStat.js";
+import Transaction from "../models/Transaction.js";
 import User from "../models/User.js";
 
 export const getUser = async (req, res) => {
@@ -5,6 +7,46 @@ export const getUser = async (req, res) => {
     const { id } = req.params;
     const user = await User.findById(id);
     res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getDashboardStats = async (req, res) => {
+  try {
+    const currentMonth = "November";
+    const currentYear = 2021;
+    const currentDay = "2021-11-15";
+
+    const transactions = await Transaction.find()
+      .limit(50)
+      .sort({ createdAt: -1 });
+    const overallStat = await OverallStat.find({ year: currentYear });
+    const {
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory,
+    } = overallStat[0];
+    const thisMonthStats = overallStat[0].monthlyData.find(
+      ({ month }) => month === currentMonth
+    );
+    const todayStats = overallStat[0].dailyData.find(
+      ({ date }) => date === currentDay
+    );
+
+    res.status(200).json({
+      transactions,
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory,
+      thisMonthStats,
+      todayStats,
+    });
   } catch (error) {
     console.log(error);
     res.status(404).json({ message: error.message });
