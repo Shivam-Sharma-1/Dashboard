c6279ca43f653edc490d90886fc20165249b79ca
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-30 19:11:15 +0530

    feat: Add new route for dashboard stats

--- server/routes/generalRoutes.js
+++ server/routes/generalRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getUser } from "../controllers/general.js";
+import { getDashboardStats, getUser } from "../controllers/general.js";
 
 const router = express.Router();
 
 router.get("/user/:id", getUser);
+router.get("/dashboard", getDashboardStats);
 
 export default router;
