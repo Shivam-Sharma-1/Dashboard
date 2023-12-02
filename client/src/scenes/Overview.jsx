ab6d2e13cac9436a5b5f8c67dd9ed6c2e08779dd
Shivam77607 <shivamsharma77607@gmail.com>
2023-12-02 13:36:02 +0530

    style: Adjust height of Box component

--- client/src/scenes/Overview.jsx
+++ client/src/scenes/Overview.jsx
@@ -7,18 +7,19 @@ function Overview() {
   const [view, setView] = useState("units");
 
   return (
-    <Box m="1.5rem 2.5rem">
+    <Box height="100%" maxHeight="85vh" m="1.5rem 2.5rem">
       <Header
         title="OVERVIEW"
         subtitle="Overview of general revenue and profit"
       />
-      <Box height="75vh">
+      <Box height="71vh">
         <FormControl sx={{ mt: "1rem" }}>
           <InputLabel>View</InputLabel>
           <Select
             value={view}
             label="View"
             onChange={(e) => setView(e.target.value)}
+            size="small"
           >
             <MenuItem value="sales">Sales</MenuItem>
             <MenuItem value="units">Units</MenuItem>
