c2d33bdbd42a821c10dd8970446352c0babda1f4
Shivam77607 <shivamsharma77607@gmail.com>
2023-12-02 13:15:09 +0530

    fix: data loading and display issue in Products component

--- client/src/scenes/Products.jsx
+++ client/src/scenes/Products.jsx
@@ -12,6 +12,8 @@ import {
   useTheme,
   useMediaQuery,
 } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
+
 import Header from "@/components/Header";
 import { useGetProductsQuery } from "@/state/api";
 
@@ -75,10 +77,10 @@ function Product({
           <Typography>id: {_id}</Typography>
           <Typography>Supply Left: {supply}</Typography>
           <Typography>
-            Yearly Sales This Year: {stat.yearlySalesTotal}
+            Yearly Sales This Year: {stat[0].yearlySalesTotal}
           </Typography>
           <Typography>
-            Yearly Units Sold This Year: {stat.yearlyTotalSoldUnits}
+            Yearly Units Sold This Year: {stat[0].yearlyTotalSoldUnits}
           </Typography>
         </CardContent>
       </Collapse>
@@ -90,49 +92,60 @@ function Products() {
   const { data, isLoading } = useGetProductsQuery();
   const isNonMobile = useMediaQuery("(min-width: 1000px)");
 
+  if (!data || isLoading)
+    return (
+      <Box
+        width="100%"
+        height="100%"
+        minHeight="80vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <CircularProgress color="secondary" />
+      </Box>
+    );
+
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="PRODUCTS" subtitle="See your list of products." />
-      {data || !isLoading ? (
-        <Box
-          mt="20px"
-          display="grid"
-          gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-          justifyContent="space-between"
-          rowGap="20px"
-          columnGap="1.33%"
-          sx={{
-            "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-          }}
-        >
-          {data.map(
-            ({
-              _id,
-              name,
-              description,
-              price,
-              rating,
-              category,
-              supply,
-              stat,
-            }) => (
-              <Product
-                key={_id}
-                _id={_id}
-                name={name}
-                description={description}
-                price={price}
-                rating={rating}
-                category={category}
-                supply={supply}
-                stat={stat}
-              />
-            )
-          )}
-        </Box>
-      ) : (
-        <>Loading...</>
-      )}
+
+      <Box
+        mt="20px"
+        display="grid"
+        gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+        justifyContent="space-between"
+        rowGap="20px"
+        columnGap="1.33%"
+        sx={{
+          "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+        }}
+      >
+        {data.map(
+          ({
+            _id,
+            name,
+            description,
+            price,
+            rating,
+            category,
+            supply,
+            stat,
+          }) => (
+            <Product
+              key={_id}
+              _id={_id}
+              name={name}
+              description={description}
+              price={price}
+              rating={rating}
+              category={category}
+              supply={supply}
+              stat={stat}
+            />
+          )
+        )}
+      </Box>
     </Box>
   );
 }
